3693083b2245b776eb5392a4864628d38ff18384
Imanthi111 <imanthij777@gmail.com>
2020-03-10 14:25:47 +0530

    First front end commit

---
+++ Chordbot-client/src/Components/SongsLoadPage.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+export class SongLoadPage extends Component{ //here we try to create home component as a class
+    render(){//here we write what should be render when the component is initiate
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+                <h3>This is the song loading page</h3>
+            </div>
+        )
+    }
+}

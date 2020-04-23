8105c24143783c1f53bb34d70888d8049c7b160b
Ayeshmantha W <ayesh99747@gmail.com>
2020-04-23 18:00:46 +0530

    Added cors

--- chordbot-server/Node API/server.js
+++ chordbot-server/Node API/server.js
@@ -1,12 +1,12 @@
 //AddModule dependencies.
 const express = require("express");
 var fileUpload = require("express-fileupload");
-
+var cors = require("cors");
 var bodyParser = require("body-parser");
 
 //Define Application object
 var app = express();
-
+app.use(cors);
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("uploads"));

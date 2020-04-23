2512f7fe3f2257fededb36c4c2afc128eeda3e30
Binuka Kamesh <33131182+Binuka97@users.noreply.github.com>
2020-04-23 22:11:03 +0530

    update chord display

--- chordbot-datascience/UpdatedVersion/Chord_Sequencer.py
+++ chordbot-datascience/UpdatedVersion/Chord_Sequencer.py
@@ -2,21 +2,21 @@ from utilities import chord_sequence
 from pickle import load
 from os import listdir
 
-models = [files for files in listdir() if files[-4:] == '.sav'] # iterate through directory and find the files with .sav format
+models = [files for files in listdir() if files[-4:] == '.sav']		#finding the files with .sav format to import
 models.sort()
 
-#print('Select model number from below list')
-#for modelNo in range(len(models)):
-#    print(modelNo + 1, ':', models[modelNo]) # print all the models one by one 
-#modelNo = int(input('Input model number\n'))
-#if modelNo > len(models) or modelNo < 1:
-#    print('Wrong input. Terminating...')
-#    exit()
+print('Select the model version: ')
+for i in range(len(models)):
+    print(i + 1, ':', models[i])
+i = int(input('Input model number\n'))
+if i > len(models) or i < 1:
+    print('Wrong input. Terminating...')
+    exit()
 
 modelNo = 1
-model = load(open(models[modelNo - 1], 'rb')) # call the trained model
+model = load(open(models[modelNo - 1], 'rb'))	#calling the trained model to execute
 wavfile = input("Enter file name with extension : ")
-#print('Chord sequence is :')
+print('The chord sequence is: ')
 for chord in chord_sequence(model, wavfile, 1):
-    print(chord, end = ' ') # display chords one by one
+    print(chord, end = ' ')		#Displaying Chords
 print()

1a49fc8def48be43f3e567ab17036766cb473078
Imanthi111 <imanthij777@gmail.com>
2020-04-26 00:03:50 +0530

    Finalized home page and chord display page

--- Chordbot-client/src/Components/ChordDisplayComponent/ChordDisplay.js
+++ Chordbot-client/src/Components/ChordDisplayComponent/ChordDisplay.js
@@ -1,71 +1,88 @@
 import React, {useState, useEffect} from "react";
 import "./ChordDisplay.css";
 import MusicPlayer from "react-responsive-music-player";
-import {
-  Row,
-  Col,
-  Card,
-  ListGroup,
-  Container,
-} from "react-bootstrap";
-import playlist from "./playlist";
+import playlist from "../../services/shapes/audio/playlist";
 import queryString from "query-string";
 import axios from "axios";
 const ChordDisplay = ({ location, match, ...props }) => {
-  //here we try to create Chord display as a functional component
-    const [chordOne,setchordOne]=useState()
-    const [chordTwo,setchordTwo]=useState()
-    const [chordThree,setchordThree]=useState()
-    const [chordFour,setchordFour]=useState()
-    const [chordFive,setchordFive]=useState()
+  //here we create Chord display as a functional component 
+  const [chords,setchords]=useState(null)
+  const [demoChords, setdemoChords] = useState(null);
+  const [selectedTrack, setSelectedtrack] = useState([
+    {
+      url: "",
+      cover: "",
+      title: "",
+      artist: [],
+    },
+  ]);
   useEffect(() => {
-    console.log(location);
     const search = location ? queryString.parse(location.search) : {};
-    fetchsongsById(search.id);
-    const { songID } = match.params;
-  }, [location.pathname]);
+    if (search.type === "playlist") {//check by the song type and run the function
+      fetchsongsById(search.id);
+    } else {
+      fetchdemosongsById(search.id);
+    }
+    playableSong(playlist, search);
+  },[location.pathname]);
+
   function fetchsongsById(url) {
-    const apiURL = `http://localhost:8000/playlistSongs/getChords/${url}`;
-    //alert("fetchsongs");
+    const apiURL = `http://localhost:8000/playlistSongs/getChords/${url}`
     axios.get(apiURL).then((res) => {
-      debugger;
-      var data = res.data;
-      console.log(res.data);
+      //var data = res.data;
+      setchords (res.data.message);
     });
   }
-  //here we write what should be return when the component is initiate
+
+  function fetchdemosongsById(urlDemo) {
+    const apiURLDemo = `http://localhost:8000/demoSongs/getChords/${urlDemo}`;
+    axios.get(apiURLDemo).then((res) => {
+      //var data = res.data;
+      console.log(res.data);
+      setdemoChords(res.data.message);
+    });
+  }
+
+  // New Fuction for Playing the song
+  function playableSong(playlist, search) {
+    playlist.map((item) => {
+      if (item.songID === search.id) {
+        setSelectedtrack([
+          {
+            url: item.url,
+            cover: item.cover,
+            title: item.title,
+            artist: item.artist,
+          },
+        ]);
+      }
+      return "";
+    });
+  }
+  // write what should be return when the component is initiate
   return (
-    <div>
-      <main>
-        <div style={{ marginLeft: "25%", marginTop: "2%" }}>
-          <MusicPlayer mode="vertical" playlist={playlist} />
-        </div>
-        <div
-          style={{
-            width: "50%",
-            border: " 5px solid #fff",
-            padding: "30px",
-            marginLeft: "25%",
-            marginTop: "3%",
-          }}
-        >
-          {/* <h1 style={{ textAlign: "left", marginLeft: "20px" }}>Chords</h1>  */}
-          <Card style={{ width: "100%", height: "50px" }}>
-            <ListGroup variant="flush">
-              <Container>
-                <Row md={4}>
-                  <Col>h</Col>
-                  <Col>hl</Col>
-                  <Col>h</Col>
-                  <Col>h</Col>
-                  <Col>h</Col>
-                </Row>
-              </Container>
-            </ListGroup>
-          </Card>
-        </div>
-      </main>
-    </div>
+    
+      <div className="mp3card">    
+          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+          <div className= "test">
+            <div className="music-info">
+              <MusicPlayer mode="vertical" playlist={selectedTrack} />
+            </div>
+          </div>
+            {chords!==null  && chords.length> 0 && chords.map((item) => 
+            
+              <div className="scrollmenu">
+                <p>{item}</p>
+              </div>  
+              
+            )}
+            {demoChords!==null  && demoChords.length> 0 && demoChords.map((item) => 
+              <div className="scrollmenu">
+                <p>{item}</p>
+              </div>     
+            )}  
+      </div>
+   
   );
 };
 export default ChordDisplay;
---
+++ Chordbot-client/src/services/shapes/audio/playlist.js
@@ -0,0 +1,97 @@
+export default [
+  {
+    songID: "1",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/jbgoode.mp3`,
+    cover:
+      "https://images-na.ssl-images-amazon.com/images/I/517Z5XXREKL._SX425_.jpg",
+    title: "Johnny B. Goode",
+    artist: ["Chuck Berry"],
+  },
+  {
+    songID: "2",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/inTheWind.mp3`,
+    cover:
+      "https://images-na.ssl-images-amazon.com/images/I/81XczPXeIjL._SL1200_.jpg",
+    title: "Blowin’ In The Wind",
+    artist: ["Bob Dylan"],
+  },
+  {
+    songID: "3",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/stairwayToHeaven.mp3`,
+    cover: "https://fringster.com/content/images/6691.jpg",
+    title: "Stairway To Heaven",
+    artist: ["Led Zeppelin"],
+  },
+  {
+    songID: "4",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/watchtowerHendrix.mp3`,
+    cover: "https://1960sdaysofrage.files.wordpress.com/2018/04/bob-dylan-all-along-the-watchtower-cbs-3.jpg",
+    title: "All Along The Watchtower",
+    artist: ["Jimi Hendrix"],
+  },
+  {
+    songID: "5",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/watchtowerHendrix.mp3`,
+    cover: "https://a.allegroimg.com/s1024/0c392d/dc25ac234fc09371ba4de3a228dc",
+    title: "What A Wonderful World",
+    artist: ["Louis Armstrong"],
+  },
+  /* {
+    songID: "1",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/imagine.mp3`,
+    cover: "https://fringster.com/content/images/6691.jpg",
+    title: "Imagine",
+    artist: ["John Lennon"],
+  },
+  {
+    songID: "2",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/heyJude.mp3`,
+    cover:
+      "https://www.the-paulmccartney-project.com/_images/artworks/hey-jude-revolution/01_big.jpg",
+    title: "Hey Jude",
+    artist: ["The Beatles"],
+  },
+  {
+    songID: "3",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/satisfaction.mp3`,
+    cover: "https://upload.wikimedia.org/wikipedia/en/7/7f/Satisfaction-us.jpg",
+    title: "(I Can’t Get No) Satisfaction",
+    artist: ["The Rolling Stones"],
+  },
+  {
+    songID: "4",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/goodVibrations.mp3`,
+    cover:
+      "https://images-na.ssl-images-amazon.com/images/I/51ER0YC5MDL._SY355_.jpg",
+    title: "Good Vibrations",
+    artist: ["The Beach Boys"],
+  },
+  {
+    songID: "5",
+    url: `${
+      window.location.origin || process.env.PUBLIC_URL
+    }/assets/mp3s/teenSpirit.mp3`,
+    cover:
+      "https://content.hungama.com/audio%20album/display%20image/300x300%20jpeg/5016117384.jpg",
+    title: "Smells Like Teen Spirit",
+    artist: ["Nirvana"],
+  }, */
+];

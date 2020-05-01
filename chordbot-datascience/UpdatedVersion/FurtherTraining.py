7e45f4bfdee88980a15e195962b9a49cd3d182d8
ChamudiVidanagama <chamudi.vidanagama@gmail.com>
2020-05-01 21:13:28 +0100

    Training the model further

--- chordbot-datascience/UpdatedVersion/FurtherTraining.py
+++ chordbot-datascience/UpdatedVersion/FurtherTraining.py
@@ -2,26 +2,24 @@ import os
 import pickle
 from PCP import pcp
 import numpy as numpy
-from utilities import convert, CtoN , NtoC
+from ChordFunctions import ChordName, ChordIndex
 from sklearn.kernel_approximation import AdditiveChi2Sampler
 
 fileName = str(input("Filename: "))
 modelName='ML_model.sav'
 mlModel = pickle.load(open(modelName, 'rb')) # read the model which is written in binary format
 addictiveSampler = AdditiveChi2Sampler()
-if fileName.rsplit('.')[-1]!='wav': # if file is not a wav file convert to mono
-	convert(fileName)
 newData = pcp(fileName) # pass file to the pcp method
 newData = numpy.array([newData])
 newData = addictiveSampler.fit_transform(newData)
 prediction = mlModel.predict(newData) # predict chord and get index
-print("Chord predicted is : ", NtoC(prediction[0]))
-ans=input("Predicted chord is correct? [yes|no]\n")
-if ans=='yes':
+print("Chord predicted is : ", ChordName(prediction[0]))
+result=input("Predicted chord is correct? [yes|no]\n")
+if result=='yes':
 	print('Model has predicted the correct chord')
 else:
 	correctChord = input("Model has predicted wrong chord. Enter the correct chord to further train the model : ")
-	correctPrediction = numpy.array([CtoN(correctChord)]) 
+	correctPrediction = numpy.array([ChordIndex(correctChord)]) 
 	if correctPrediction != prediction: # check whether model predicted chord and userinput chord are equal
 		mlModel.partial_fit(newData, correctPrediction)
 		pickle.dump(mlModel, open(modelName, 'wb')) # overwrite model in binary format

1cc11373ffe172fd5a416b62a0f5b8ead58e0a2e
Ayeshmantha W <ayesh99747@gmail.com>
2020-05-02 19:23:44 +0530

    edited frontend

--- Chordbot-client/src/Components/ChordDisplayComponent/ChordDisplay.js
+++ Chordbot-client/src/Components/ChordDisplayComponent/ChordDisplay.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./ChordDisplay.css";
-import MusicPlayer from "react-responsive-music-player";
+import MusicPlayer from "react-responsive-music-player"; //music player lib
 import playlist from "../../services/shapes/audio/playlist";
 import demoSongs from "../../services/shapes/audio/demoSongs";
 import queryString from "query-string";
@@ -12,6 +12,7 @@ const ChordDisplay = ({ location, match, ...props }) => {
   const [chords, setchords] = useState(null);
   const [demoChords, setdemoChords] = useState(null);
   const [selectedTrack, setSelectedtrack] = useState([
+    //functions to update selected song track and store data
     {
       url: "",
       cover: "",
@@ -20,16 +21,16 @@ const ChordDisplay = ({ location, match, ...props }) => {
     },
   ]);
   useEffect(() => {
-    const search = location ? queryString.parse(location.search) : {}; 
+    const search = location ? queryString.parse(location.search) : {}; //ternary to grab song details from url
     if (search.type === "playlist") {
-      //check by the song type and run the function
+      //condition to check by the song type and run the function
       fetchsongsById(search.id);
       playableSong(playlist, search);
     } else {
       fetchdemosongsById(search.id);
       playableSong(demoSongs, search);
     }
-  }, [location.pathname]);
+  }, [location, location.pathname]);
 
   function fetchsongsById(url) {
     const apiURL = `http://localhost:8000/playlistSongs/getChords/${url}`;
@@ -37,6 +38,7 @@ const ChordDisplay = ({ location, match, ...props }) => {
       setchords(res.data.message);
     });
   }
+
   function fetchdemosongsById(urlDemo) {
     const apiURLDemo = `http://localhost:8000/demoSongs/getChords/${urlDemo}`;
     axios.get(apiURLDemo).then((res) => {
@@ -44,6 +46,7 @@ const ChordDisplay = ({ location, match, ...props }) => {
       setdemoChords(res.data.message);
     });
   }
+
   // New Fuction for Playing the song
   function playableSong(playlist, search) {
     playlist.map((item) => {
@@ -61,7 +64,6 @@ const ChordDisplay = ({ location, match, ...props }) => {
     });
   }
 
-
   // write what should be return when the component is initiate
   return (
     <div className="mp3card">
@@ -74,28 +76,25 @@ const ChordDisplay = ({ location, match, ...props }) => {
           <MusicPlayer mode="vertical" playlist={selectedTrack} />
         </div>
       </div>
-
-      {chords !== null && !isEmpty(chords) ? (
+      {chords !== null && !isEmpty(chords) ? ( //use isEmpty to fetch the data is loading and display them
         chords.map((item) => (
           <div className="scrollmenu">
             <p>{item}</p>
           </div>
         ))
       ) : (
-        <ReactBootStrap.Spinner className="spinner" animation="border" />
+        <ReactBootStrap.Spinner className="spinner" animation="border" /> //If data is still loading display a spinner
       )}
 
-
-      {demoChords !== null && !isEmpty(demoChords) ? (
+      {demoChords !== null && !isEmpty(demoChords) ? ( //use isEmpty to fetch the data is loading and display them
         demoChords.map((item) => (
           <div className="scrollmenu">
             <p>{item}</p>
           </div>
         ))
       ) : (
-        <ReactBootStrap.Spinner className="spinner" animation="border" />
-      )} 
-
+        <ReactBootStrap.Spinner className="spinner" animation="border" /> //If data is still loading display a spinner
+      )}
     </div>
   );
 };
--- Chordbot-client/src/Components/HomeComponent/Home.js
+++ Chordbot-client/src/Components/HomeComponent/Home.js
@@ -1,38 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"; //how it would be converted to a functional component with hooks
 import queryString from "query-string";
-import isEmpty from "lodash/isEmpty";
-import {
-  Row,
-  Col,
-  Image,
-  Card,
-  ListGroup,
-  Container,
-} from "react-bootstrap";
-import axios from "axios";//connecting apis using axios library
+import isEmpty from "lodash/isEmpty"; //lodash library
+import { Row, Col, Image, Card, ListGroup, Container } from "react-bootstrap";
+import axios from "axios"; //connecting apis using axios library
 import "./Home.css";
 const Home = (props) => {
-  //create home component
-  const [playListSongs, setPlayListsonglist] = useState([]);
-  const [demoSongs, setDemoSongs] = useState([]);
-
-  useEffect(() => { //calling functions for APIs
+  //create home functional component
+  const [playListSongs, setPlayListsonglist] = useState([]); //function to update playlist songs and store it in an array
+  const [demoSongs, setDemoSongs] = useState([]); //function to update demo songs and store it in an array
+  useEffect(() => {
+    //calling functions of APIs
     fetchPlayListSongs();
     fetchDemoSongs();
   }, []);
-
   async function fetchPlayListSongs() {
-    const apiURL = "http://localhost:8000/playlistSongs/getSongs";//url to get the playlist song detail
-    const response = await axios.get(apiURL);//append details by getting from the data list
+    const apiURL = "http://localhost:8000/playlistSongs/getSongs"; //url to get the playlist song detail
+    const response = await axios.get(apiURL); //append details by getting from the data list
     setPlayListsonglist(response.data);
   }
-
   async function fetchDemoSongs() {
     const apiURL = "http://localhost:8000/demoSongs/getSongs";
     const response = await axios.get(apiURL);
     setDemoSongs(response.data);
   }
-
   return (
     <div>
       <div id="background">
@@ -40,8 +30,9 @@ const Home = (props) => {
           <div className="container-fluid">
             <div className="song-card">
               <div className="song-cont">
-                <img alt="img"
-                  src="https://chordify.net/img/channels/getting-started.jpg"
+                <img
+                  alt="img"
+                  src="https://s3.envato.com/files/80988932-1e08-46dd-ba12-17feef229434/inline_image_preview.jpg"
                   frameborder="0"
                 />
                 <p>
@@ -53,7 +44,7 @@ const Home = (props) => {
               </div>
               <div className="song-desc">
                 <h2>Playlist</h2>
-                {!isEmpty(playListSongs)//validate songs are not empty, function from loadash library
+                {!isEmpty(playListSongs) //Check if value is not empty.
                   ? playListSongs.map((item) => {
                       //we gonna map the card blocks it is like a for loop
                       return (
@@ -62,9 +53,11 @@ const Home = (props) => {
                           style={{ width: "100%" }}
                           onClick={() =>
                             props.history.push(
+                              //redirecting the user to another route chord display page
                               `/ChordDisplay/?${queryString.stringify({
-                                id: item.songID,
-                                type: 'playlist'
+                                //Serialize an object to a query string
+                                id: item.songID, //assign song id and song type to it
+                                type: "playlist",
                               })}`
                             )
                           }
@@ -73,19 +66,28 @@ const Home = (props) => {
                             <Container>
                               <Row md={4}>
                                 <Col xs={2} md={2}>
-                                  {" "}
-                                <Image src={"https://www.pinclipart.com/picdir/middle/493-4936384_free-png-colorful-music-note-png-png-image.png"}style={{height:"60px",width:"60px"}} rounded />
+                                  <Image
+                                    src={
+                                      "https://www.pinclipart.com/picdir/middle/493-4936384_free-png-colorful-music-note-png-png-image.png"
+                                    }
+                                    style={{ height: "60px", width: "60px" }}
+                                    rounded
+                                  />
                                 </Col>
-                                <Col xs={4} md={4}>{item.songName}</Col>
-                                <Col >{item.artist}</Col>
-                                <Col xs={4} md={4}>{item.genre}</Col>
+                                <Col xs={4} md={4}>
+                                  {item.songName}
+                                </Col>
+                                <Col>{item.artist}</Col>
+                                <Col xs={4} md={4}>
+                                  {item.genre}
+                                </Col>
                               </Row>
                             </Container>
                           </ListGroup>
                         </Card>
                       );
                     })
-                  : ""}
+                  : "Cannot Load playlist songs!!"}
               </div>
             </div>
           </div>
@@ -113,25 +115,31 @@ const Home = (props) => {
                             <Container>
                               <Row md={4}>
                                 <Col xs={2} md={2}>
-                                  {" "}
-                                  <Image src={"https://www.pinclipart.com/picdir/middle/493-4936384_free-png-colorful-music-note-png-png-image.png"}
-                                  style={{height:"60px",width:"60px"}} rounded 
+                                  <Image
+                                    src={
+                                      "https://www.pinclipart.com/picdir/middle/493-4936384_free-png-colorful-music-note-png-png-image.png"
+                                    }
+                                    style={{ height: "60px", width: "60px" }}
+                                    rounded
                                   />
                                 </Col>
-                                <Col xs={3} md={3}>{item.songName}</Col>
+                                <Col xs={3} md={3}>
+                                  {item.songName}
+                                </Col>
                                 <Col>{item.artist}</Col>
-                                <Col >{item.genre}</Col>
+                                <Col>{item.genre}</Col>
                               </Row>
                             </Container>
                           </ListGroup>
                         </Card>
                       );
                     })
-                  : ""}
+                  : "Cannot Load demo songs!!"}
               </div>
               <div className="song-cont">
-                <img  alt="img"
-                src="https://learnenglishteens.britishcouncil.org/sites/teens/files/field/image/istock_000016090616small_0.jpg"
+                <img
+                  alt="img"
+                  src="https://learnenglishteens.britishcouncil.org/sites/teens/files/field/image/istock_000016090616small_0.jpg"
                   frameborder="0"
                 />
                 <p>

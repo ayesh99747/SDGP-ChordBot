38b655adb48a4f2fe4ec375449c53ff54252dc9c
Ayeshmantha W <ayesh99747@gmail.com>
2020-05-04 02:00:15 +0530

    Update PCP.py

--- chordbot-datascience/PCP.py
+++ chordbot-datascience/PCP.py
@@ -1,25 +1,22 @@
-import numpy as np
+import numpy as numpy
 import scipy.io.wavfile
 from os import listdir
 from os.path import isfile, join
-from utilities import convStoM
 
-def pcp(path) :
-    fs,y = scipy.io.wavfile.read(path) # read the wav file
-    if len(y.shape) == 2 :
-        y = convStoM(y)
-    n = np.size(y)
-    k = int(n/2)
-    y = (np.square(abs(np.fft.rfft(y))[:k]))
-    pcp = np.zeros(12)
-    fref = 130.8
-    M = np.zeros(k)
-    M[0] = -1
-    for l in range(1, k) :
-        M[l] = round(12*np.log2((fs/fref)*(l/n)))%12
-    for i in range(0, 12) :
-        pcp[i] = np.dot(y, (M==(i*np.ones(k))))
-    if sum(pcp)==0:
-        return np.zeros(12, dtype=float64)
-    pcp = pcp/sum(pcp)
-    return pcp
+def pcp(filePath) :
+    sampleRate,data = scipy.io.wavfile.read(filePath) # read the wav file
+    newData = numpy.size(data) 
+    half = int(newData/2)
+    data = (numpy.square(abs(numpy.fft.rfft(data))[:half]))# represent using DFT
+    pcpArray = numpy.zeros(12)
+    const = 130.8
+    Mo = numpy.zeros(half) 
+    Mo[0] = -1
+    for count in range(1, half) :
+        Mo[count] = round(12*numpy.log2((sampleRate/const)*(count/newData)))%12 # fit to 12 tones
+    for tones in range(0, 12) :
+        pcpArray[tones] = numpy.dot(data, (Mo==(tones*numpy.ones(half)))) # get PCP values
+    if sum(pcpArray)==0:
+        return numpy.zeros(12, dtype=float64)
+    pcpArray = pcpArray/sum(pcpArray)
+    return pcpArray # return PCP values

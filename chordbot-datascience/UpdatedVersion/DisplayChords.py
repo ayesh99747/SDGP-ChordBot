73ee651d3627fef190e7dca1162c1f5c22447d36
Binuka Kamesh <33131182+Binuka97@users.noreply.github.com>
2020-05-03 23:50:30 +0530

    Updated a function calling

--- chordbot-datascience/UpdatedVersion/DisplayChords.py
+++ chordbot-datascience/UpdatedVersion/DisplayChords.py
@@ -1,4 +1,4 @@
-from ChordFunctions import extractChords
+from ChordFunctions import findAllChords
 from os import listdir
 from pickle import load
 
@@ -7,6 +7,6 @@ Load_MLmodel = load(open(modelName, 'rb'))		#loading the model to start the chor
 
 userSong = input("Input the wav file alongwith correct extention : ")	#user input of the wav song file
 print('Chord progression of the requested song is: ')
-for pattern in extractChords(Load_MLmodel, userSong):	#calling the function from ChordFunctions to get the chord pattern
+for pattern in findAllChords(userSong,Load_MLmodel):	#calling the function from ChordFunctions to get the chord pattern
     print(pattern, end = ' ')							#Displaying the chords pattern of the song
 print()
